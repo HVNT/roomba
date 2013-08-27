88df938d7d23e24bb8f4b755b3e69fb7a883449c
Spencer Applegate <spencer.applegate3@gmail.com>
2013-08-27 09:58:31 -0400

    Updated npm packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -419,10 +419,22 @@ module.exports = function (grunt) {
                 },
                 environment: 'demo'
             }
+        },
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= yeoman.dist %>/scripts/{,*/}*.js',
+                        '<%= yeoman.dist %>/styles/{,*/}*.css',
+                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        '<%= yeoman.dist %>/styles/fonts/*'
+                    ]
+                }
+            }
         }
     });
-
-    grunt.loadNpmTasks('grunt-hustler');
+//
+//    grunt.loadNpmTasks('grunt-hustler');
 
     grunt.renameTask('regarde', 'watch');
 
