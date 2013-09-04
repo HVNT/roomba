8e1f228f7cf521bce2b51eb8a53f5d9abea1577a
Spencer Applegate <spencer.applegate3@gmail.com>
2013-09-04 13:30:54 -0400

    Moved config files to app-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -136,6 +136,7 @@ module.exports = function (grunt) {
                     src: [
                         'app/**/*',
                         'components/**/*',
+                        'app-config/**/*',
                         'core/**/*',
                         'img/**/*',
                         '*.html*'
