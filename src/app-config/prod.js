10c48794c62821cf02e8d09af2a3e1d2ad4e8bd8
apledger <apledger3@gmail.com>
2013-09-18 14:34:08 -0400

    attempted production build

--- src/app-config/prod.js
+++ src/app-config/prod.js
@@ -11,7 +11,7 @@
 angular.module('rescour.config', [])
     .factory('$_api', function () {
         var url = {
-                prod: "https://api.maasive.net/v2/rescour"
+                prod: "http://roomba.rescour.com/"
             },
             stripeTokens = {
                 test: 'pk_test_wSAqQNQKI7QqPmBpDcQLgGM7',
